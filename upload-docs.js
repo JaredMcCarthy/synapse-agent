b03b0e52693257559327b7709bb9e69d24e20bce
jared <jared.lopezmccarthy05@gmail.com>
2025-11-22 22:30:21 -0600

    feauture nueva para subir archivos, pero aun no me sale la api de openai en langbase, ya se abrio un error

--- upload-docs.js
+++ upload-docs.js
@@ -1,17 +1,14 @@
-//1
+
 import 'dotenv/config';
 import {Langbase} from 'langbase'
 import {readFile} from 'fs/promises'
 import path from 'path'
-import {readFileSync} from "node:fs";
 
-//2
-const Langbase = new Langbase({
+
+const langbase = new Langbase({
     apiKey: process.env.LANGBASE_API_KEY
 })
 
-
-//3
 async function main(){
     const cwd = process.cwd();
     const memoryName = 'knowledge-base';
@@ -19,6 +16,20 @@ async function main(){
     //esta es la que va leer los faqfiles
     const langbaseFaq = await readFile(path.join(cwd, 'docs', 'langbase.faq.txt'))
 
+    // subimos los archivos a la memoria de langbase
+    const faqResult = await langbase.memories.documents.upload({
+        memoryName,
+        contentType: 'text/html',
+        documentName: 'langbase.faq.txt',
+        document: langbaseFaq,
+        meta: {category: 'Support', topic: 'Langbase FAQs'},
+    });
+
+    console.log(faqResult.ok ? 'Has been succesfull uploaded' : 'We had an issue with the upload');
 }
 
+main()
 
+
+// DOC que use poara guiarme
+// https://langbase.com/docs/sdk/memory/document-upload?sdk=typescript
