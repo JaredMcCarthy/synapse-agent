ba66f78674d0ad59ab1fd21be90158d39bc3d2cc
jared <jared.lopezmccarthy05@gmail.com>
2026-01-17 00:15:01 -0600

    agregando chunks mas query del usuario hacia el model ai de langbase que se envia

---
+++ index.ts
@@ -0,0 +1,15 @@
+
+import {runMemoryAgent, runAiSupportAgent} from "./agents";
+
+async function main(){
+    const query = 'How do i upgrade to individual plan?'
+    const chunks = await runMemoryAgent(query)
+
+    const completion  = await runAiSupportAgent({
+        chunks,
+        query,
+    })
+    console.log(completion)
+}
+
+main()
